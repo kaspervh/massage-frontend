62d1b0af73cc2cd86b88ed5aac348805ec3da329
kaspervh <kaspervhauschildt@gprotonmail.com>
2021-04-15 10:17:50 +0200

    made quick fixes to footer and booking page

--- src/components/PrivacyPolicyComponent.js
+++ src/components/PrivacyPolicyComponent.js
@@ -1,6 +1,10 @@
-import React from "react"
+import React, {useEffect} from "react"
 
 const PrivacyPolicyComponent = () => {
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [])
+  
   return(
     <div className="container">
       <section>
